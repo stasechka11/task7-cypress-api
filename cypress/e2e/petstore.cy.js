5bee26a8845850042f2434a324464014b17570cb
Anastasiia Fokina <stasechka11@yandex.ru>
2024-07-16 14:11:02 +0300

    Added tests

--- cypress/e2e/petstore.cy.js
+++ cypress/e2e/petstore.cy.js
@@ -1,10 +1,37 @@
 import user from "../fixtures/user.json";
+import updatedUser from "../fixtures/user-update.json";
 
 describe('PetStore API tests', () => {
 
   it('Should create user', () => {
     cy.request("POST", "/user", user).then((response) => {
+      cy.log(JSON.stringify(response.body));
       expect(response.status).be.eql(200);
-    })
-  })
-})
+    });
+
+    cy.request("GET",`/user/${user.username}`,).then((response) => {
+    cy.log(JSON.stringify(response.body));
+    expect(response.status).be.eql(200);
+    expect(response.body).be.eql(user);
+    });
+  });
+
+  it('Should update user', () => {
+    cy.request("POST", "/user", user).then((response) => {
+      cy.log(JSON.stringify(response.body));
+      expect(response.status).be.eql(200);
+    });
+
+    cy.request("PUT", `/user/${user.username}`, updatedUser).then((response) => {
+      cy.log(JSON.stringify(response.body));
+      expect(response.status).be.eql(200);
+    });
+
+    cy.request("GET",`/user/${updatedUser.username}`).then((response) => {
+    cy.log(JSON.stringify(response.body));
+    expect(response.status).be.eql(200);
+    expect(response.body).be.eql(updatedUser);
+    });
+  });
+
+});
